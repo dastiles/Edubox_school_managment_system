5740126d48f7559dbe27e0d6d28bd81e3f16a2e7
charles madhuku <dastilesforever@gmail.com>
2023-07-25 18:57:06 +0200

    params fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,14 +3,12 @@ import getUserInfor from '@/app/actions/getUserInfor'
 import Header from '@/app/components/header/Header'
 import ProfileWrapper from '@/app/components/profile/ProfileWrapper'
 import Sidebar from '@/app/components/sidebar/Sidebar'
-import { userInfo } from 'os'
-import React from 'react'
 
-interface Iparams {
-    userId: string
-}
 
-const Profile = async ({ params }: { params: Iparams }) => {
+
+
+
+const Profile = async () => {
     const currentUser = await getCurrentUser()
     const getUser = await getUserInfor()
     return (
